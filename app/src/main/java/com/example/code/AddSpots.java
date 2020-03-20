40eb273159c94af73ad005c0c6fb1778ce8c7091
Muizz Adeni <muizzadeni@hotmail.com>
2020-03-20 04:02:20 +0000

    Adding locations to database

--- app/src/main/java/com/example/code/AddSpots.java
+++ app/src/main/java/com/example/code/AddSpots.java
@@ -17,7 +17,9 @@ public class AddSpots extends AppCompatActivity {
      EditText EditLongitude;
      EditText EditLatitude;
      EditText EditTown;
+     Button button;
      DatabaseReference reff;
+     Spots spots;
 
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
@@ -26,22 +28,24 @@ public class AddSpots extends AppCompatActivity {
         EditTown = findViewById(R.id.enter_town);
         EditLongitude = findViewById(R.id.enter_longitude);
         EditLatitude = findViewById(R.id.enter_latitude);
-       // reff= FirebaseDatabase.getInstance().getReference().child(String.valueOf(EditTown)); // EditTown is town name
+        button = (Button)findViewById(R.id.add_location);
+        spots = new Spots();
+        reff= FirebaseDatabase.getInstance().getReference().child("Spots");
 
-        Button button = findViewById(R.id.add_location);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 double Longitude = Double.parseDouble(EditLongitude.getText().toString().trim()); //might need to change this as double is being insterted
                 double Latitude = Double.parseDouble(EditLatitude.getText().toString().trim()); //might need to change this as double is being insterted
-                String Town = EditTown.getText().toString().trim(); //this was done differently in video
-/*
-                reff.push().setValue(Town);
-                reff.push().setValue(Longitude);
-                reff.push().setValue(Latitude);
-                Toast.makeText(AddSpots.this,"data insterted successfully", Toast.LENGTH_LONG).show(); //done till here
-*/
+
+                spots.setTown(EditTown.getText().toString().trim());
+                spots.setLatitude(Latitude);
+                spots.setLongitude(Longitude);
+
+                reff.push().setValue(spots);
+                Toast.makeText(AddSpots.this, "data insterted successfully",Toast.LENGTH_LONG).show();
+
             }
         });
 
---
+++ app/src/main/java/com/example/code/Spots.java
@@ -0,0 +1,35 @@
+package com.example.code;
+
+public class Spots {
+
+    private String Town;
+    private Double Latitude;
+    private Double Longitude;
+
+    public Spots() {
+    }
+
+    public String getTown() {
+        return Town;
+    }
+
+    public void setTown(String town) {
+        Town = town;
+    }
+
+    public Double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        Latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        Longitude = longitude;
+    }
+}
