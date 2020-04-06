0f16bbbea0b9018612547e378dd47e5bd7e90916
Muizz Adeni <muizzadeni@hotmail.com>
2020-04-06 13:26:35 -0700

    retrieving town name from database completed

--- app/src/main/java/com/example/code/TownList.java
+++ app/src/main/java/com/example/code/TownList.java
@@ -33,11 +33,11 @@ import java.util.ArrayList;
 @SuppressLint("Registered")
 public class TownList extends AppCompatActivity {
 
-   ListView myListView;
+    ListView myListView;
 
-   ArrayList<String> myArrayList = new ArrayList<>();
+    ArrayList<String> myArrayList = new ArrayList<>();
 
-   DatabaseReference mRef;
+    DatabaseReference mRef;
 
     @Override
 
@@ -52,12 +52,13 @@ public class TownList extends AppCompatActivity {
         myListView = (ListView) findViewById(R.id.list); //List is id given in xml file
         myListView.setAdapter(myArrayAdapter);
 
-        mRef = FirebaseDatabase.getInstance().getReference();
+        mRef = FirebaseDatabase.getInstance().getReference("Spots");
 
         mRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                String value = dataSnapshot.getValue(String.class); // this will receive string values from firebase
+                String value = dataSnapshot.child("town").getValue(String.class); // this will receive string values from firebase
+                //reff.child(String.valueOf(EditTown.getText())).setValue(spots);
                 myArrayList.add(value);
                 myArrayAdapter.notifyDataSetChanged();
             }
@@ -87,7 +88,98 @@ public class TownList extends AppCompatActivity {
     }
 }
 
+// --------------------=========================================================================================
 
+//package com.example.code;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//
+//import android.annotation.SuppressLint;
+//import android.os.Bundle;
+//import android.content.Intent;
+//
+//import android.os.Bundle;
+//
+////import android.support.v7.app.AppCompatActivity;
+//
+//import android.view.View;
+//
+//import android.widget.AdapterView;
+//
+//import android.widget.ArrayAdapter;
+//
+//import android.widget.ListView;
+//
+//import com.google.firebase.database.ChildEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//import java.util.ArrayList;
+//
+//
+//@SuppressLint("Registered")
+//public class TownList extends AppCompatActivity {
+//
+//    ListView myListView;
+//
+//    ArrayList<String> myArrayList = new ArrayList<>();
+//
+//    DatabaseReference mRef;
+//
+//    @Override
+//
+//    protected void onCreate(Bundle savedInstanceState) {
+//
+//        super.onCreate(savedInstanceState);
+//
+//        setContentView(R.layout.listview_item_click_event);
+//
+//        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(TownList.this, android.R.layout.simple_list_item_1,myArrayList);
+//
+//        myListView = (ListView) findViewById(R.id.list); //List is id given in xml file
+//        myListView.setAdapter(myArrayAdapter);
+//
+//        mRef = FirebaseDatabase.getInstance().getReference();
+//
+//        mRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                String value = dataSnapshot.getValue(String.class); // this will receive string values from firebase
+//                myArrayList.add(value);
+//                myArrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//                myArrayAdapter.notifyDataSetChanged();
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//    }
+//}
+
+// ----------------==============================================================================
 
 //package com.example.code;
 //
